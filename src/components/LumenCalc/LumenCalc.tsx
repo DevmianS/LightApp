1f392a2d6c720c4f9c68a45a8f0a2ed80f517df1
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-07-13 21:04:41 +0200

    Initialized Sass

---
+++ src/components/LumenCalc/LumenCalc.tsx
@@ -0,0 +1,10 @@
+import cl from './LumenCalc.module.scss';
+const LumenCalc = () => {
+  return (
+    <div className={cl.calc}>
+      <p>Here will be lumen calc</p>
+    </div>
+  );
+};
+
+export default LumenCalc;
---
+++ src/components/UI/Panel.tsx
@@ -0,0 +1,5 @@
+const Panel = () => {
+  <div>PANEL</div>;
+};
+
+export default Panel;
